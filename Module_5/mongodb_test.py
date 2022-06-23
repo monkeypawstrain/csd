150a67cce3c7eac648bea682936a8090be70992b
Rufino Tzunun <rtzununboc@my365.bellevue.edu>
2022-06-23 14:53:11 -0700

    check

--- Module_5/mongodb_test.py
+++ Module_5/mongodb_test.py
@@ -8,10 +8,3 @@ db = client.pytech
 print("--Pytech Collection List")
 print(db.list_collection_names())
 input("\n\n End of the program,press any key to exit.. ")
-#students = client.pytech.get_collection("students")
-#student = {"_id":1003,
-#"first_name": "Rufino"}
-#new_student_id = students.insert_one(student).inserted_id
-#print(new_student_id)
-
-#print(db.list_collection_names())W

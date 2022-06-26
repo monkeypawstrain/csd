e4e4e2d2e7c93cc203a728df5c59c49052020aba
monkeypawstrain <102393713+monkeypawstrain@users.noreply.github.com>
2022-06-26 08:19:40 -0700

    Update pytech_insert.py

--- Module_5/pytech_insert.py
+++ Module_5/pytech_insert.py
@@ -1,6 +1,6 @@
 import certifi
 from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.ptdopkt.mongodb.net/pytech?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+url = "mongodb+srv://admin:admin@cluster0.nwh5cip.mongodb.net/retryWrites=true&w=majority"
 ca = certifi.where()
 client = MongoClient(url,tlsCAFile=ca)
 students = client.pytech.get_collection("students")

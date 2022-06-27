561a0843f96aa6f93ae3f7181ae0825570b818ca
Rufino Tzunun <rtzununboc@my365.bellevue.edu>
2022-06-26 17:12:45 -0700

    up

--- Module_5/mongodb_test.py
+++ Module_5/mongodb_test.py
@@ -6,7 +6,7 @@ Module 5
 from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.nwh5cip.mongodb.net/retryWrites=true&w=majority",
 client = MongoClient(url)
-db = client.pyech
+db = client.pytech
 print("--Pytech Collection List")
 print(db.list_collection_names())
 input("\n\n End of the program,press any key to exit.. ")

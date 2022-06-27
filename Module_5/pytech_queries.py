d2239004ceb612f489780efbf4c0465dfa88fc41
Rufino Tzunun <rtzununboc@my365.bellevue.edu>
2022-06-26 17:09:12 -0700

    up

--- Module_5/pytech_queries.py
+++ Module_5/pytech_queries.py
@@ -9,18 +9,18 @@ client = MongoClient(url)
 db = client.pytech
 students = db.students
 
-student_list = students.find({})
+docs = students.find({})
 
 print('\n  -- Displaying Students Documents From find() QUERY --')
 
-for doc in student_list:
+for doc in docs:
     
     print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
-brand = students.find_one({"student_id": "1008"})
+doc = students.find_one({"student_id": "1008"})
 
 print('\n-- Displaying Student Document From find_one() QUERY --')
 
-print("  Student ID: " + brand["student_id"] + "\n  First Name: " + brand["first_name"] + "\n  Last Name: " + brand["last_name"] + "\n")
+print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
 
 input('\n\nEnd of program, press any key to continue... ')
